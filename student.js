2bf42f5b60b7a15e6821033a55301ba91aacc580
josephzhao20 <98077809+josephzhao20@users.noreply.github.com>
2023-11-03 00:09:23 -0500

    Create student.js

---
+++ student.js
@@ -0,0 +1,15 @@
+class Student {
+    constructor(name, choices) {
+        this.name = name;
+        this.choices = choices;
+        this.group = null;
+    }
+
+    assignToGroup(group) {
+        this.group = group;
+    }
+
+    hasChoiceInGroup(group) {
+        return this.choices.some(choice => group.getMemberNames().includes(choice));
+    }
+}
